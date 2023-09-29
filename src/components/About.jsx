b87e64a709e557588976a49f34bada92475a8c51
eldhose <eldho1998@gmail.com>
2023-09-29 10:33:07 +0100

    updated build

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -42,7 +42,7 @@ function About() {
               <p className="lead">ലോകമെമ്പാടുമുള്ള മലയാളം വായനക്കാരുമായി ജീവന്റെ വചനം പങ്കുവയ്ക്കുന്നതിനാണ് ഈ വെബ്സൈറ്റ് സൃഷ്ടിച്ചിരിക്കുന്നത്.</p>
               <p>ഈ വെബ്‌സൈറ്റിൽ നൽകിയിരിക്കുന്ന മലയാളം പരിഭാഷ എടുത്തിരിക്കുന്നത് <a target="_blank" href="https://github.com/godlytalias/Bible-Database/tree/master/Malayalam">ഇവിടെ നിന്നാണ്</a>. അതിൽ നിന്ന് വായിച്ചപ്പോൾ കണ്ട മലയാളത്തിലെ അക്ഷരതെറ്റുകൾ മറ്റും തിരുത്തിയിട്ടാണ് ഇവിടെ കൊടുത്തിരിക്കുന്നത്. ഈ വെബ്സൈറ്റിൻറെ കോഡ് കിട്ടുന്നതിനായി <a target="_blank" href="https://github.com/eldhosejoys/bible-app-vite">ഗിറ്റ്ഹബ് സന്ദർശിക്കുക</a>.</p>
               <p>ചില പുസ്തകങ്ങൾക്ക് ശീർഷകങ്ങൾ ഇപ്പോൾ കൊടുത്തുകൊണ്ടിരിക്കുകയാണ്. അത് സഹായകരമായിരിക്കും എന്ന് പ്രതീഷിക്കുന്നു. ശീർഷകങ്ങൾ കൊടുത്ത പുസ്തകങ്ങൾ താഴെ കൊടുത്തിരിക്കുന്നു. </p>
-              <div className="container row">{cards}</div>
+              <p className="container row">{cards}</p>
               <p>കൂടുതൽ വായിക്കുക: <a target="_blank" href={`https://blog.${window.location.hostname}`}>ബ്ലോഗ്</a> , <a target="_blank" href={`https://parrudeesa.${window.location.hostname}`}>വചന വെളിപ്പാടുകൾ</a></p>
               {buildDate &&
                 <p><small>Last Updated: {buildDate}</small></p>
